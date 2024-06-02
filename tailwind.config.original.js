cf01f51dd189dee571d6ae90f1b93ae88348a5da
Bersen Naidoo <bersen.naidoo@gmail.com>
2024-06-02 14:58:33 +0200

    Generate full tailwind config

---
+++ tailwind.config.original.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "src/**/*.{html,md,11ty.js,liquid,njk,hbs,mustache,ejs,haml,pug}",],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
